2597211b756eefc8be073902d58586e4bf0a9c5b
fareez <iqmal3@outlook.com>
2025-09-24 03:42:20 +0800

    🚨 fix deprecated members

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -266,7 +266,7 @@ class _HomePageState extends State<HomePage> {
                                       ],
                                       onChanged: (value) async =>
                                           _setFromSurah(value),
-                                      value: fromSurah,
+                                      initialValue: fromSurah,
                                     );
                                   },
                                 ),
@@ -356,7 +356,7 @@ class _HomePageState extends State<HomePage> {
                                           toSurah = value;
                                         });
                                       },
-                                      value: toSurah,
+                                      initialValue: toSurah,
                                     );
                                   },
                                 ),
